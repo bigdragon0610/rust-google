c322d42ec959d3263c86c57b5a5e5e3dd89b2ec2
ryudai <bigdragon.06102001@gmail.com>
2022-12-29 04:12:52 +0900

    perf: :zap: 検索の高速化
    wait_until_navigatedを別スレッドで起動し、遷移が完了するまで順次データを取り出す

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use ansi_term::{Color, Style};
 use clap::Parser;
 use headless_chrome::Browser;
-use std::error::Error;
+use std::{error::Error, sync::Arc, thread};
 
 #[derive(Parser)]
 struct Cli {
@@ -15,39 +15,61 @@ fn main() {
 }
 
 fn browse(input: String) -> Result<(), Box<dyn Error>> {
-    let browser = Browser::default()?;
+    let browser = Browser::default().unwrap();
 
-    let tab = browser.wait_for_initial_tab()?;
+    let tab = browser.wait_for_initial_tab().unwrap();
+    let rc_cnt = Arc::strong_count(&tab);
 
-    tab.navigate_to(&input)?.wait_until_navigated()?;
+    tab.navigate_to(&input).unwrap();
 
-    let div_elements = tab.wait_for_elements("div.tF2Cxc")?;
+    let mut completed_element_ids: Vec<u32> = Vec::new();
 
-    let mut output = "\n".to_string();
-    for div_element in div_elements {
-        let mut title = "".to_string();
-        let mut url = "".to_string();
-        let mut overview = "".to_string();
-
-        let h3_element = div_element.find_element("h3.LC20lb");
-        if h3_element.is_ok() {
-            title = h3_element?.get_inner_text()?;
-            url = div_element.find_element("a")?.get_attributes()?.unwrap()[1].to_string();
+    let tab_clone = tab.clone();
+    thread::spawn(move || {
+        tab_clone.wait_until_navigated().unwrap();
+    });
+    loop {
+        let mut is_last = false;
+        if Arc::strong_count(&tab) == rc_cnt {
+            is_last = true;
         }
-        let overview_div = div_element.find_element("div.VwiC3b");
-        if overview_div.is_ok() {
-            overview = overview_div?.get_inner_text()?;
-        }
+        let div_elements = tab.wait_for_elements("div.tF2Cxc").unwrap();
+        for div_element in div_elements {
+            let id = div_element.backend_node_id;
+            if completed_element_ids.contains(&id) {
+                continue;
+            }
 
-        let title = Style::new().fg(Color::Yellow).bold().paint(title);
-        let url = Style::new().fg(Color::Green).paint(url);
-        if overview != "" {
-            overview += "\n";
+            let h3_element = div_element.find_element("h3.LC20lb");
+            if h3_element.is_err() {
+                break;
+            }
+            let title = h3_element.unwrap().get_inner_text().unwrap();
+
+            let a_element = div_element.find_element("a");
+            if a_element.is_err() {
+                break;
+            }
+            let url = a_element.unwrap().get_attributes().unwrap().unwrap()[1].to_string();
+            let mut overview = String::new();
+
+            let overview_div = div_element.find_element("div.VwiC3b");
+            if overview_div.is_ok() {
+                overview = overview_div.unwrap().get_inner_text().unwrap();
+            }
+
+            let title = Style::new().fg(Color::Yellow).bold().paint(title);
+            let url = Style::new().fg(Color::Green).paint(url);
+            if overview != "" {
+                overview += "\n";
+            }
+            println!("{}\n{}\n{}", title, url, overview);
+            completed_element_ids.push(id);
         }
-        output += format!("{}\n{}\n{}\n", title, url, overview).as_str();
+        if is_last {
+            break;
+        }
     }
-
-    println!("{}", output);
 
     Ok(())
 }
